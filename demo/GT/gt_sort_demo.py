a5e0ede6a066d5a234bf72bb7a62db60814d6636
chongxi lai <chongxi.lai@gmail.com>
2018-06-07 16:40:03 -0400

    get delete_spk function in GT notebook

--- demo/GT/gt_sort_demo.py
+++ demo/GT/gt_sort_demo.py
@@ -5,20 +5,20 @@ import numpy as np
 from PyQt5.QtWidgets import QApplication
 import sys
 
-prb = probe(shank_no=1)
-prb[0] = np.array([0,1,2])
-prb.mapping[0] = np.array([-10,0])
-prb.mapping[1] = np.array([10,0])
-prb.mapping[2] = np.array([0,10])
-prb.fs = 20000.
-prb.n_ch = 3
-prb.reorder_by_chip=False
+tritrode = probe(shank_no=1)
+tritrode[0] = np.array([0,1,2])
+tritrode.mapping[0] = np.array([-10,0])
+tritrode.mapping[1] = np.array([10,0])
+tritrode.mapping[2] = np.array([0,10])
+tritrode.fs = 20000.
+tritrode.n_ch = 3
+tritrode.reorder_by_chip=False
 
-model = MainModel('./cell_0109.bin', './cell_0109.spk.bin', prb, binary_radix=11)
+model = MainModel('./cell_0109.bin', './cell_0109.spk.bin', tritrode, binary_radix=11)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    view = MainView(prb)
+    view = MainView(tritrode)
     view.set_data(0, model.mua, model.spk[0], model.fet[0], model.clu[0])
     view.show()
     sys.exit(app.exec_())
