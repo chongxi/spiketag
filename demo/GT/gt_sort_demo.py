b668298a56641c88e5916067c90a08b5214d9412
chongxi <chongxi.lai@gmail.com>
2018-06-08 00:39:11 -0400

    update gt_sort_demo.py

--- demo/GT/gt_sort_demo.py
+++ demo/GT/gt_sort_demo.py
@@ -1,6 +1,7 @@
 from spiketag.base import *
 from spiketag.view import *
 from spiketag.mvc import MainModel, MainView
+from spiketag.mvc.Control import controller
 import numpy as np
 from PyQt5.QtWidgets import QApplication
 import sys
@@ -14,11 +15,17 @@ tritrode.fs = 20000.
 tritrode.n_ch = 3
 tritrode.reorder_by_chip=False
 
-model = MainModel('./cell_0109.bin', './cell_0109.spk.bin', tritrode, binary_radix=11)
-
-if __name__ == '__main__':
-    app  = QApplication(sys.argv)
+def model_view():
+    model = MainModel('./cell_0109.bin', './cell_0109.spk.bin', tritrode, binary_radix=11)
     view = MainView(tritrode)
     view.set_data(0, model.mua, model.spk[0], model.fet[0], model.clu[0])
     view.show()
+
+def sorter():
+    ctrl = controller('./cell_0109.bin', './cell_0109.spk.bin', tritrode, binary_radix=11)
+    ctrl.show()
+
+if __name__ == '__main__':
+    app  = QApplication(sys.argv)
+    sorter()
     sys.exit(app.exec_())
