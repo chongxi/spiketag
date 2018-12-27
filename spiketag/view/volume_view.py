9ebabb6d1964f9d7ff894ee0fb90cad3e63b4567
chongxi lai <chongxi.lai@gmail.com>
2018-12-27 01:20:08 -0500

    volume_view now has nested viewbox with border color as cluster color

--- spiketag/view/volume_view.py
+++ spiketag/view/volume_view.py
@@ -9,16 +9,18 @@ from ..utils import Timer
 
 class volume_view(scene.SceneCanvas):
     
-    def __init__(self, show=False, debug=False):
-        scene.SceneCanvas.__init__(self, keys=None)
+    def __init__(self, nvbs=1, show=False, debug=False):
+        scene.SceneCanvas.__init__(self, size=(800, 600), keys='interactive')
 
         self.unfreeze()
-        self.view = self.central_widget.add_view()
-        self.view.camera = 'turntable'
+        self.grid = self.central_widget.add_grid()
 
-        self.vol = scene.visuals.Volume()
-        self.vol.unfreeze()
-        self.view.add(self.vol)
+        self.vbs = {}
+        self.vol = {}
+        self.data = {}
+
+        self.init_vb(nvbs)
+        
         # self._timer = app.Timer(0.0)
         # self._timer.connect(self.on_timer)
         # self._timer.start()
@@ -30,11 +32,20 @@ class volume_view(scene.SceneCanvas):
         if show is True:
             self.show()
 
+    def init_vb(self, nvbs):
+        for i in range(nvbs):
+            self.vbs[i] = self.grid.add_view(name='vb'+str(i), border_color=palette[i])
+            self.vbs[i].camera = 'turntable'
+        for i in range(nvbs):
+            self.vol[i] = scene.visuals.Volume()
+            self.vol[i].unfreeze()
+            self.vbs[i].add(self.vol[i])
 
 
-    def set_data(self, data, clim=None):
+    def set_data(self, data, vb_id=0, clim=None):
         # volumetric data for example: data.shape = (128,128,128)
 
-        self.data = data
-        self.vol.set_data(data)
-
+        self.data[vb_id] = data
+        self.vol[vb_id].set_data(data, clim)
+        cam = scene.cameras.TurntableCamera(parent=self.vbs[vb_id].scene, fov=60)
+        self.vbs[vb_id].camera = cam
