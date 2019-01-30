168174e576af326f959a10e044b7acb4f57f181d
chongxi <chongxi.lai@gmail.com>
2019-01-30 17:25:54 -0500

    test several mode (sequantial, parallel) of background clustering

--- spiketag/cpu/__init__.py
+++ spiketag/cpu/__init__.py
@@ -1,6 +1,6 @@
 import ipyparallel as ipp
 import numpy as np
-
+from vispy import app
 
 dpgmm_def = '''
 def dpgmm(max_cluster_n=10):
@@ -35,14 +35,17 @@ class multicore(object):
 
     def _exec(self, cpu_id, func_exe, *args):
         cmd = func_exe.format(args[0])
-        self.cpu_ready[cpu_id] = self.cpu[cpu_id].execute(cmd, block=False)
+        self.cpu_ready[cpu_id] = self.cpu[cpu_id].execute(cmd)
 
     def test_cpu_ready(self, cpu_id):
         return self.cpu_ready[cpu_id].ready()
 
     @property
     def cpu_ready_list(self):
-        return [self.test_cpu_ready(cpu_id) for cpu_id in range(self.cpu_No)]
+        try:
+            return [self.test_cpu_ready(cpu_id) for cpu_id in range(self.cpu_No)]
+        except:
+            return [True]*self.cpu_No
 
     def get(self, cpu_id, var):
         return self.cpu[cpu_id].pull(var).get()
@@ -53,11 +56,14 @@ class multicore(object):
 
 class DPGMM_IPY(multicore):
     """docstring for DPGMM_multicore"""
-    def __init__(self, cpu_No, fet):
+    def __init__(self, cpu_No):
         super(DPGMM_IPY, self).__init__(cpu_No)
         for i in range(cpu_No):
-            self.set_data(i, {'data': fet[i]})
             self._import(i, dpgmm_def)
+
+    def __call__(self, fet):
+        for i in range(self.cpu_No):
+            self.set_data(i, {'data': fet[i]})
         
     def run(self, group_id):
         self._exec(group_id, dpgmm_exec, 4)
@@ -67,6 +73,18 @@ class DPGMM_IPY(multicore):
             self._exec(i, dpgmm_exec, 4)
 
         
+def report_cpu_status(ev):
+    group_id = dpgmm[0]['group_id']
+    print('cpu ready list:', dpgmm.cpu_ready_list)
+    if dpgmm.cpu_ready_list[0]:
+        dpgmm[0]['data'] = fet[group_id]
+        dpgmm[0].execute('label = dpgmm(4)')
+        labels = dpgmm[0]['label']
+        print(group_id, labels)
+    dpgmm[0]['group_id'] += 1
+    if dpgmm[0]['group_id'] == group_No:
+        timer.stop()
+        app.quit()
 
 
 if __name__ == '__main__':
@@ -84,11 +102,26 @@ if __name__ == '__main__':
     #     print('cpu'+str(i), ar)
 
 
-    # 2. Test DPGMM_IPY
-    fet = np.random.randn(8, 10000, 4)
-    dpgmm = DPGMM_IPY(cpu_No=8, fet=fet)
+    # 2. Test sequential background execution
+    # cpu_No = 1
+    # group_No = 8
+    # fet = np.random.randn(group_No, 10000, 4)
+    # dpgmm = DPGMM_IPY(cpu_No=cpu_No)
+    # dpgmm(fet)
+    # dpgmm[0]['group_id'] = 0
+    # timer = app.Timer(connect=report_cpu_status, interval=0.5)
+    # timer.start()
+    # app.run()
+    
+
+    # 3. Test parallel background execution
+    cpu_No = 8
+    group_No = 8
+    fet = np.random.randn(group_No, 10000, 4)
+    dpgmm = DPGMM_IPY(cpu_No=cpu_No)
+    dpgmm(fet)
     dpgmm.run_all()
     print('cpu ready list:', dpgmm.cpu_ready_list)
-    for cpu in dpgmm:
-        print(cpu['label'])
+    for i in range(dpgmm.cpu_No):
+        print(dpgmm[i]['label'])
     print('cpu ready list:', dpgmm.cpu_ready_list)
