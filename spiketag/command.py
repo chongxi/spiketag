f548c54272f108130c3a781461b52e10adf58398
chongxi <chongxi.lai@gmail.com>
2019-03-13 22:47:42 -0400

    solve spiketa command line speed issue by not import spiketag globally

--- spiketag/command.py
+++ spiketag/command.py
@@ -7,15 +7,12 @@ def main():
 @main.command()
 @click.argument('binaryfile')
 @click.argument('probefile')
-@click.option('--nch', prompt='nch', default='160')
-@click.option('--fs', prompt='fs', default='25000.0')
-@click.option('--nbits', prompt='nbits', default='16')
+@click.option('--nbits', prompt='nbits', default='32')
 @click.option('--time', prompt='time', default='0')
-@click.option('--span', prompt='span', default='5')
-def check(binaryfile, probefile, nch, fs, nbits, time, span):
-    # from spiketag import view_data
-    click.echo('spiketag-check {}:{} channels, {}bits, time={}, span={}'.format(binaryfile, nch, nbits, time, span))
-    # view_data(binaryfile, probefile, int(nch), float(fs), int(nbits), float(time), float(span))
+@click.option('--span', prompt='span', default='10')
+def check(binaryfile, probefile, nbits, time, span):
+    from .spiketag import view_data
+    view_data(binaryfile, probefile, int(nbits), float(time), float(span))
 
 @main.command()
 def sort():
--- spiketag/spiketag.py
+++ spiketag/spiketag.py
@@ -4,14 +4,13 @@ from spiketag.fpga import xike_config
 from vispy import app
 
 
-# def view_data(filename, prbfile, nCh, fs, nbits, time=0, span=5):
-#     prb = probe()
-#     prb.load(prbfile)
-#     prb.n_ch = nCh
-#     prb.fs  = fs
-#     mua = MUA(filename, prb, numbytes=nbits//8, scale=False)
-#     mua.show(chs=prb.chs[:128], span=span, time=time)
-#     app.run()
+def view_data(filename, prbfile, nbits, time=0, span=5):
+    prb = probe()
+    prb.load(prbfile)
+    mua = MUA(filename, prb, numbytes=nbits//8, scale=False)
+    if span == -1: span=None
+    mua.show(chs=prb.chs[:128], span=span, time=time)
+    app.run()
 
 
 def check_fpga(prbfile, var):
