9667a25d9aad25d07796e3df28e1936ee9806dc4
chongxi <chongxi.lai@gmail.com>
2019-10-09 21:03:08 -0400

    minor bug fixed with clean API for NaiveBayes

--- spiketag/analysis/decoder.py
+++ spiketag/analysis/decoder.py
@@ -150,7 +150,7 @@ class NaiveBayes(Decoder):
         self.pc.get_fields(self.pc.spk_time_dict, self.train_time[0], self.train_time[1], rank=False)
 
     def predict(self, X):
-        post_2d = bayesian_decoding(self.fields, X, t_window=self.t_window)
+        post_2d = bayesian_decoding(self.pc.fields, X, t_window=self.t_window)
         binned_pos = argmax_2d_tensor(post_2d)
         y = smooth(self.pc.binned_pos_2_real_pos(binned_pos), 5)
         return y
