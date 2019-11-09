4456d7b939e59a9ae05a3d47fa994a847a7d7018
chongxi <chongxi.lai@gmail.com>
2019-11-09 15:49:39 -0500

    demo spike raster

---
+++ demo/view_test/raster_view.py
@@ -0,0 +1,12 @@
+from spiketag.view import raster_view
+import numpy as np
+
+fet_packet = np.fromfile('./fet.bin', dtype=np.int32).reshape(-1,7)
+spkid_packet = fet_packet[:, [0,-1]]
+spkid_packet = np.delete(spkid_packet, np.where(spkid_packet[:,1]==0), axis=0)
+
+rsview = raster_view()
+rsview.title = 'spike raster'
+rsview.set_data(spkid_packet)
+rsview.show()
+rsview.app.run()
