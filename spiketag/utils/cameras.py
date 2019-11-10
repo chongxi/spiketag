d333a1a745727bf6c9441f1ae6ed4be8131a6191
chongxi lai <chongxi.lai@gmail.com>
2019-11-09 22:49:27 -0500

    XSyncCamera

--- spiketag/utils/cameras.py
+++ spiketag/utils/cameras.py
@@ -1,6 +1,28 @@
 from vispy.scene import BaseCamera
 from vispy.geometry import Rect
 
+class XSyncCamera(BaseCamera):
+    '''
+        Camera only be sync the X coordinate, not sync with Y coordinate.
+    '''
+    def set_state(self, state=None, **kwargs):
+        D = state or {}
+        if 'rect' not in D:
+            return
+        for cam in self._linked_cameras:
+            r = Rect(D['rect'])
+            if cam is self._linked_cameras_no_update:
+                continue
+            try:
+                cam._linked_cameras_no_update = self
+                cam_rect = cam.get_state()['rect']
+                r.top = cam_rect.top
+                r.bottom = cam_rect.bottom
+                cam.set_state({'rect':r})
+            finally:
+                cam._linked_cameras_no_update = None
+
+                
 class YSyncCamera(BaseCamera):
     '''
         Camera only be sync the Y coordinate, not sync with X coordinate.
