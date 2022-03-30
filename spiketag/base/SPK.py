aabf2ddf7a4c50f3ad1f4e1b121a53e957711653
chongxi <chongxi.lai@gmail.com>
2022-03-30 00:04:15 -0400

    spk.to_spikedf() to genereate a spike dataframe that can be directly load by `pc`

--- spiketag/base/SPK.py
+++ spiketag/base/SPK.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numexpr as ne
+import pandas as pd
 # from spiketag.view import spike_view
 from .FET import FET
 from ..utils.conf import info
@@ -116,6 +117,20 @@ def _to_fet(_spk_array, _weight_vector, method='weighted-pca', ncomp=6, whiten=F
 
 class SPK():
     def __init__(self, spkdict=None):
+        '''
+        Example:
+            from spiketag.base import SPK, FET, CLU
+            spk = SPK()
+            spk.load_spkwav('./spk_wav.bin')   # load spk_wav.bin
+            spk_df = spk.sort(method='dpgmm')  # sort spk_wav.bin by dpgmm
+
+        Variables:
+            spk.ch, 
+            spk.spk_time, 
+            spk.electrode_group, 
+            spk.spk_dict, 
+            spk_spk_time_dict
+        '''
         if spkdict is not None:
             self.spkdict = spkdict
             self(self.spkdict)
@@ -176,14 +191,16 @@ class SPK():
         spk.load_spkwav('./spk_wav.bin')        
         '''
         spk = np.fromfile(file, dtype=np.int32).reshape(-1, 20, 4)
-        ch, spk_time, electrode_group = spk[..., 0, 1], spk[..., 0, 2], spk[..., 0, 3]
-        group_list = np.sort(np.unique(electrode_group))
+        self.ch, self.spk_time, self.electrode_group = spk[..., 0, 1], spk[..., 0, 2], spk[..., 0, 3]
+        group_list = np.sort(np.unique(self.electrode_group))
         self.spk_dict = {}
+        self.spk_time_dict = {}
         for group in group_list:
-            self.spk_dict[group] = spk[electrode_group == group][:, 1:, :]/(2**14)
+            self.spk_dict[group] = spk[self.electrode_group == group][:, 1:, :]/(2**14) # grouped spike waveforms
+            self.spk_time_dict[group] = spk[self.electrode_group == group][:, 0, 2]
         self(self.spk_dict)
 
-    def tofet(self, group_id=None, method='pca', ncomp=6, whiten=False):
+    def tofet(self, group_id=None, method='pca', ncomp=4, whiten=False):
         fet = {}
         # pca_comp = {}
         # shift = {}
@@ -197,7 +214,38 @@ class SPK():
             #     info('spk._tofet(group_id={}, method={}, ncomp={}, whiten={})'.format(group, method, ncomp, whiten))
             # info('----------------success------------------')
             # info(' ')
-            return FET(fet)
+            self.fet = FET(fet)
+            return self.fet
+    
+    def sort(self, minimum_spks=50, cluster_method='dpgmm', file=None):
+        self.tofet(method='pca', ncomp=4, whiten=False);
+        ### todo: 1. add blocking mode    2. kmeans for clusterless sort
+        self.fet.to_clu(method=cluster_method, mode='blocking')
+        ### todo: 3. merge all low spike number cluster ( < minimum_spks(e.g., 50) spikes ) to 0 cluster (noise)
+        self.fet.assign_clu_global_labels()
+        self.to_spikedf(file)
+
+    def to_spikedf(self, file=None):
+        '''
+        spike_df is a dataframe that each row is a spike packet (frame_id, group_id, fet0, fet1, fet2, fet3, spike_id)
+        sorted by timestamps
+        '''
+        spk_matrix = np.array([]).reshape(-1, 7)
+        for g in self.fet.group:
+            h = np.hstack((self.spk_time_dict[g].reshape(-1,1),                         # spike frame_id (time stamps in #samples)
+                           self.electrode_group[self.electrode_group==g].reshape(-1,1), # spike group_id (electrode group)
+                           self.fet[g][:,:4],                                           # spike features (multichannel waveform 4d feature)
+                           self.fet.clu[g].membership_global.reshape(-1,1)))            # spike spike_id (assigned unit id)
+            spk_matrix = np.append(spk_matrix, h, axis=0)
+
+        self.spike_df = pd.DataFrame(spk_matrix)
+        self.spike_df.columns = ['frame_id', 'group_id', 'fet0', 'fet1', 'fet2', 'fet3', 'spike_id']
+        self.spike_df = self.spike_df.sort_values('frame_id')
+
+        if file is not None:
+            self.spike_df.to_pickle(file, index=False)
+
+        return self.spike_df
 
     # def show(self, group_id):
     #     self.spk_view = spike_view()
