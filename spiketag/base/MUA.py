12af6f2f9bbfe37c0d620a0140bc8d484fbee1dd
chongxi <chongxi.lai@gmail.com>
2022-07-17 18:08:56 -0400

    comment out several unused methods

--- spiketag/base/MUA.py
+++ spiketag/base/MUA.py
@@ -193,58 +193,57 @@ class MUA(object):
         info(' ')               
         return SPK(self.spkdict)
 
-    def get_nid(self, corr_cutoff=0.95):  # get noisy spk id
-        # 1. dump spikes file (binary)
-        piv = self.pivotal_pos.T
-        nspk = self.pivotal_pos.shape[1]
-        # the reason adding mod operation here is if the spike is in the very end ,i.e: within 15 offset to end 
-        # point, this will make self.data[rows, :] out of bound.
-        rows = (np.arange(-10,15).reshape(1,-1) + piv[:,0].reshape(-1,1)) % self.data.shape[0]
-        cols = piv[:,1].reshape(-1,1)
-        full_spk = self.data[rows, :]
-        filename = os.path.dirname(self.filename)+'/.'+os.path.basename(self.filename)+'.spkfull'
-        full_spk.tofile(filename)
+    # def get_nid(self, corr_cutoff=0.95):  # get noisy spk id
+    #     # 1. dump spikes file (binary)
+    #     piv = self.pivotal_pos.T
+    #     nspk = self.pivotal_pos.shape[1]
+    #     # the reason adding mod operation here is if the spike is in the very end ,i.e: within 15 offset to end 
+    #     # point, this will make self.data[rows, :] out of bound.
+    #     rows = (np.arange(-10,15).reshape(1,-1) + piv[:,0].reshape(-1,1)) % self.data.shape[0]
+    #     cols = piv[:,1].reshape(-1,1)
+    #     full_spk = self.data[rows, :]
+    #     filename = os.path.dirname(self.filename)+'/.'+os.path.basename(self.filename)+'.spkfull'
+    #     full_spk.tofile(filename)
 
-        # 2. parallel screen the noise id out, then gather from CPUs
-        from ipyparallel import Client
-        from ipyparallel.util import interactive
-        rc = Client()
-        cpu = rc[:]
-        cpu.block = True
+    #     # 2. parallel screen the noise id out, then gather from CPUs
+    #     from ipyparallel import Client
+    #     from ipyparallel.util import interactive
+    #     rc = Client()
+    #     cpu = rc[:]
+    #     cpu.block = True
 
-        @cpu.remote(block=True)      # to be executed by cpu
-        @interactive                 # to be on the global()
-        def get_noise_ids(filename, corr_cutoff, n_group):
-            spk_data = np.memmap(filename, dtype='f4').reshape(-1, 25, n_group)
-            noise_id = []
-            # corr_cutoff = 0.98
-            # ind is index assign to each cpu
-            # corr_cutoff is threshold of corr_coef
-            for i in ind:
-                spikes = spk_data[i]
-                seq = np.abs(np.corrcoef(spikes[5:15, 16:].T).ravel())
-                seq[seq>corr_cutoff] = 1
-                seq[seq<corr_cutoff] = 0
-                if np.median(seq) == 1.0:
-                    noise_id.append(i)
-            return noise_id
+        # @cpu.remote(block=True)      # to be executed by cpu
+        # @interactive                 # to be on the global()
+        # def get_noise_ids(filename, corr_cutoff, n_group):
+        #     spk_data = np.memmap(filename, dtype='f4').reshape(-1, 25, n_group)
+        #     noise_id = []
+        #     # corr_cutoff = 0.98
+        #     # ind is index assign to each cpu
+        #     # corr_cutoff is threshold of corr_coef
+        #     for i in ind:
+        #         spikes = spk_data[i]
+        #         seq = np.abs(np.corrcoef(spikes[5:15, 16:].T).ravel())
+        #         seq[seq>corr_cutoff] = 1
+        #         seq[seq<corr_cutoff] = 0
+        #         if np.median(seq) == 1.0:
+        #             noise_id.append(i)
+        #     return noise_id
 
         # f = interactive(get_noise_ids)
-        cpu.execute('import numpy as np')
-        cpu.scatter('ind', range(nspk))
-        noise_id = get_noise_ids(filename, corr_cutoff, self.probe.n_group)
-        # cpu.execute("%reset")
-        try:
-            os.remove(filename)
-        except OSError:
-            pass
-        return np.hstack(np.asarray(noise_id))
+        # cpu.execute('import numpy as np')
+        # cpu.scatter('ind', range(nspk))
+        # noise_id = get_noise_ids(filename, corr_cutoff, self.probe.n_group)
+        # # cpu.execute("%reset")
+        # try:
+        #     os.remove(filename)
+        # except OSError:
+        #     pass
+        # return np.hstack(np.asarray(noise_id))
 
-
-    def remove_high_corr_noise(self, corr_cutoff=0.95):
-        nid = self.get_nid(corr_cutoff)
-        self.pivotal_pos = np.delete(self.pivotal_pos, nid, axis=1)
-        info('removed noise ids: {} '.format(nid)) 
+    # def remove_high_corr_noise(self, corr_cutoff=0.95):
+    #     nid = self.get_nid(corr_cutoff)
+    #     self.pivotal_pos = np.delete(self.pivotal_pos, nid, axis=1)
+    #     info('removed noise ids: {} '.format(nid)) 
 
     # def remove_groups_under_fetlen(self, fetlen):
     #     ids = []
@@ -258,14 +257,13 @@ class MUA(object):
     #     self.pivotal_pos = np.delete(self.pivotal_pos, ids, axis=1)
     #     info('removed all spks on these groups: {}'.format(groups)) 
 
-    def group_spk_times(self):
-        group_with_times = {}
-        for g in range(self.probe.n_group):
-            pos   = self.pivotal_pos[0][np.in1d(self.pivotal_pos[1], pivotal_chs)]
-            times = self.pivotal_pos[0][np.where(np.in1d(self.pivotal_pos[1],self.probe[g]))[0]]
-            if len(times) > 0: group_with_times[g] = times
-        return group_with_times
-
+    # def group_spk_times(self):
+    #     group_with_times = {}
+    #     for g in range(self.probe.n_group):
+    #         pos   = self.pivotal_pos[0][np.in1d(self.pivotal_pos[1], pivotal_chs)]
+    #         times = self.pivotal_pos[0][np.where(np.in1d(self.pivotal_pos[1],self.probe[g]))[0]]
+    #         if len(times) > 0: group_with_times[g] = times
+    #     return group_with_times
 
     def show(self, chs, span=None, time=0):
         '''
